646990980812e147410b6faf0620d5bf3e34cadf
Timéo Soëte <106147830+symplyyy@users.noreply.github.com>
2026-05-22 15:26:54 +0200

    exo05: implémentation du code

--- 05_exo/src/main/java/com/testunitaire/DiceScore.java
+++ 05_exo/src/main/java/com/testunitaire/DiceScore.java
@@ -9,6 +9,16 @@ public class DiceScore {
     }
 
     public int getScore() {
-        return 0;
+        int scoreFirst = de.getRoll();
+        int scoreSecond = de.getRoll();
+
+        if (scoreFirst == scoreSecond) {
+            if (scoreFirst == 6) {
+                return 30;
+            }
+            return scoreFirst * 2 + 10;
+        } else {
+            return scoreFirst < scoreSecond ? scoreSecond : scoreFirst;
+        }
     }
 }
