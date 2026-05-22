cd405e6457061e2e17d395677ce940eb902f8130
Timéo Soëte <106147830+symplyyy@users.noreply.github.com>
2026-05-22 15:50:38 +0200

    exo06: implémentation du code

--- 06_exo/src/main/java/com/coworking/RechercheVille.java
+++ 06_exo/src/main/java/com/coworking/RechercheVille.java
@@ -1,5 +1,6 @@
 package com.coworking;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +16,23 @@ public class RechercheVille {
     }
 
     public List<String> Rechercher(String mot) {
-        return List.of();
+        if (mot.equals("*")) {
+            return villes;
+        }
+
+        if (mot.length() < 2) {
+            throw new NotFoundException("Il faut au moins 2 caractères");
+        }
+
+        List<String> resultat = new ArrayList<>();
+        String recherche = mot.toLowerCase();
+
+        for (String ville : villes) {
+            if (ville.toLowerCase().contains(recherche)) {
+                resultat.add(ville);
+            }
+        }
+
+        return resultat;
     }
 }
